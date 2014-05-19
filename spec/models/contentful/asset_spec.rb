c4db4daf44d76fb0bb71f65b0190a75aa5760444
Mohamed Magdy <mohamed.magdy@sumup.com>
2014-05-19 16:48:53 +0200

    Requiring spec_helper in contentful resource models tests

--- spec/models/contentful/asset_spec.rb
+++ spec/models/contentful/asset_spec.rb
@@ -1,28 +1,28 @@
-module Contentful
-  describe Asset do
-    let(:contentful_asset) do
-      client = Contentful::Client.new(
-        space: "cfexampleapi",
-        access_token: "b4c0n73n7fu1"
-      )
+require 'spec_helper'
 
-      client.asset("nyancat")
-    end
+describe Contentful::Asset do
+  let(:contentful_asset) do
+    client = Contentful::Client.new(
+      space: "cfexampleapi",
+      access_token: "b4c0n73n7fu1"
+    )
 
-    context "display asset fields" do
-      it "should return openstruct that responds with main fields" do
-        expect(
-          contentful_asset.display.publication_date
-        ).to eq(contentful_asset.updated_at)
+    client.asset("nyancat")
+  end
 
-        expect(
-          contentful_asset.display.title
-        ).to eq(contentful_asset.sys[:id])
+  context "display asset fields" do
+    it "should return openstruct that responds with main fields" do
+      expect(
+        contentful_asset.display.publication_date
+      ).to eq(contentful_asset.updated_at)
 
-        expect(
-          contentful_asset.display.description
-        ).to eq("#{contentful_asset.fields[:title]}, #{contentful_asset.fields[:description]}")
-      end
+      expect(
+        contentful_asset.display.title
+      ).to eq(contentful_asset.sys[:id])
+
+      expect(
+        contentful_asset.display.description
+      ).to eq("#{contentful_asset.fields[:title]}, #{contentful_asset.fields[:description]}")
     end
   end
 end
--- spec/models/contentful/deleted_entry_spec.rb
+++ spec/models/contentful/deleted_entry_spec.rb
@@ -1,24 +1,24 @@
-module Contentful
-  describe DeletedEntry do
-    let(:contentful_deleted_entry) do
-      client = Contentful::Client.new(
-        space: "cfexampleapi",
-        access_token: "b4c0n73n7fu1"
-      )
+require 'spec_helper'
 
-      client.entry("nyancat")
-    end
+describe Contentful::DeletedEntry do
+  let(:contentful_deleted_entry) do
+    client = Contentful::Client.new(
+      space: "cfexampleapi",
+      access_token: "b4c0n73n7fu1"
+    )
 
-    context "display deleted entry fields" do
-      it "should return openstruct that responds with main fields" do
-        expect(
-          contentful_deleted_entry.display.publication_date
-        ).to eq(contentful_deleted_entry.updated_at)
+    client.entry("nyancat")
+  end
 
-        expect(
-          contentful_deleted_entry.display.title
-        ).to eq(contentful_deleted_entry.sys[:id])
-      end
+  context "display deleted entry fields" do
+    it "should return openstruct that responds with main fields" do
+      expect(
+        contentful_deleted_entry.display.publication_date
+      ).to eq(contentful_deleted_entry.updated_at)
+
+      expect(
+        contentful_deleted_entry.display.title
+      ).to eq(contentful_deleted_entry.sys[:id])
     end
   end
 end
